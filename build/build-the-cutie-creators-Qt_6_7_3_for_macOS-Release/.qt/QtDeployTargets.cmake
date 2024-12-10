bac77410c5eeeba857b851a9508464f47ddab11b
Tiffany Huang <goldentogawhale1258@gmail.com>
2024-12-09 22:53:33 -0500

    refactor(repo): delete build folder

--- build/build-the-cutie-creators-Qt_6_7_3_for_macOS-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_the-cutie-creators_FILE /Users/Tiffany/Desktop/csci1230/the-cutie-creators/build/build-the-cutie-creators-Qt_6_7_3_for_macOS-Release/the-cutie-creators)
-set(__QT_DEPLOY_TARGET_the-cutie-creators_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_StaticGLEW_FILE /Users/Tiffany/Desktop/csci1230/the-cutie-creators/build/build-the-cutie-creators-Qt_6_7_3_for_macOS-Release/libStaticGLEW.a)
-set(__QT_DEPLOY_TARGET_StaticGLEW_TYPE STATIC_LIBRARY)
